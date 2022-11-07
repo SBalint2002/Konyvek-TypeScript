2c74a8abf27a01680a9d49700bbbd0c02e644751
Sárándi <sarandi.balint@gmail.com>
2022-11-07 14:45:56 +0100

    form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let konyvek = [];
+document.addEventListener("DOMContentLoaded", () => {
+    var _a;
+    document.getElementById("paperbook").style.display = "none";
+    (_a = document.getElementById("valasztas")) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        if (document.getElementById('ebookradio').checked) {
+            document.getElementById("paperbook").style.display = "none";
+            document.getElementById("ebook").style.display = "block";
+        }
+        else if (document.getElementById('paperbookradio').checked) {
+            document.getElementById("ebook").style.display = "none";
+            document.getElementById("paperbook").style.display = "block";
+        }
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,19 @@ export interface Book {
     kiir() : void;
     toString() : string;
 }
+
+let konyvek: Book[] = [];
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    (document.getElementById("paperbook") as HTMLElement).style.display = "none";
+    document.getElementById("valasztas")?.addEventListener('click', () => {
+        if((document.getElementById('ebookradio') as HTMLInputElement).checked){
+            (document.getElementById("paperbook") as HTMLElement).style.display = "none";
+            (document.getElementById("ebook") as HTMLElement).style.display = "block";
+        }else if((document.getElementById('paperbookradio') as HTMLInputElement).checked){
+            (document.getElementById("ebook") as HTMLElement).style.display = "none";
+            (document.getElementById("paperbook") as HTMLElement).style.display = "block";
+        }
+    })
+});
